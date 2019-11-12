d4a25ccd99b195eafe59ab3d0fdbb7a5ade5e01c
Lucas Henrique <lucashenrique.pereira@hotmail.com>
2019-11-11 23:50:51 -0300

    Perfil de usuario

--- src/screens/User/User.tsx
+++ src/screens/User/User.tsx
@@ -1,16 +1,14 @@
 import auth from "@react-native-firebase/auth";
 import firestore from "@react-native-firebase/firestore";
 import React, { useState, useEffect } from "react";
-import { Button } from "react-native-paper";
 import {
-  Text,
   FlatList,
   View,
   TouchableOpacity,
   StyleSheet,
   Alert
 } from "react-native";
-import { Overlay, Input, Rating, Icon } from "react-native-elements";
+import { Overlay, Input, Rating, Icon, Divider, Button, Text } from "react-native-elements";
 import Loading from "../../components/Loading";
 import MLoading from "../../components/ModalLoading";
 import { showMessage } from "react-native-flash-message";
@@ -19,26 +17,83 @@ import * as Service from "./Service";
 const ref = firestore().collection("usuarios");
 
 const user = ()=>{
-let [Usuario, setUsuario] = useState({});
+let [Nome, setNome] = useState('');
+let [Sobrenome, setSobrenome] = useState('');
+let [Celular, setCelular] = useState('');
+let [Email, setEmail] = useState('');
+let [IsEditable, setIsEditable] = useState(false);
 useEffect(()=>{
 let usuarioAtual = ref.doc(auth().currentUser.uid);
 usuarioAtual.get().then(doc =>{
-    setUsuario(doc.data());
-
+    const {nome, sobrenome, email, celular} = doc.data()
+    setNome(nome);
+    setSobrenome(sobrenome);
+    setCelular(celular)
+    setEmail(email)
 })
 }, [])
 
-console.log(Usuario);
 
 
+const changeEditable = () =>{
+  setIsEditable(!IsEditable)
+}
 
 return(
-    <View>
-      <Text>  Informações do usuario</Text>
+    <View style = {styles.container}>
+      <Text h2 style={styles.headerText}>  Informações do usuario</Text>
+      <Input onChangeText = {(e) => setNome(e)} label= "Nome" value={Nome} disabled = {IsEditable} rightIcon={
+        <Icon
+        name = "edit"
+        type = "material"
+        onPress = {changeEditable}
+        /> 
+      } />
+      <Input onChangeText = {(e) => setSobrenome(e)} label= "Sobrenome" value={Sobrenome} disabled = {IsEditable} rightIcon={
+        <Icon
+        name = "edit"
+        type = "material"
+        onPress = {changeEditable}
+        /> 
+      } />
 
-      <Text>  Email: {Usuario.email? Usuario.email : ""}</Text>
-      <Text>  Telefone: {Usuario.celular? Usuario.celular : ""}</Text>
+      <Input onChangeText = {(e) => setCelular(e)} label= "Telefone" options={{
+            maskType: "BRL",
+            withDDD: true,
+            dddMask: "(99) "
+          }}  type={"cel-phone"} keyboardType="phone-pad" value={Celular} disabled = {IsEditable} rightIcon={
+        <Icon
+        name = "edit"
+        type = "material"
+        onPress = {changeEditable}
+        /> 
+      } />
+
+      <Input label= "Email" value={Email} disabled = {true} />
+      <Button title="Atualizar" disabled={IsEditable}/> 
+      <View style={styles.containerBotao}>
+      <Button buttonStyle={styles.botao} title="Ofertados"/>  
+      <Button buttonStyle={styles.botao} title="Favoritos"/>
+      </View>
     </View>
 )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f6b93b"
+  },
+  containerBotao: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  botao:{
+    marginLeft: "20%"
+  },
+  headerText: {
+    textAlign: "center" 
+  },
+});
+
 export default user
